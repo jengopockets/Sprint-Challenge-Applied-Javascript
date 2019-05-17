3dc17fedd9f4273437d05a5074746a161624bf7f
Jengopockets <jengopockets@gmail.com>
2019-05-17 13:05:49 -0400

    Debuged MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,31 +4,27 @@ class TabLink {
     this.tabElement = tabElement;
     
     // Get the `data-tab` value from this.tabElement and store it here
-    this.tabData = this.tabElement.dataset.tab; 
-  
+    this.tabData = this.tabElement.dataset.tab;
+    // document.querySelector(`.tab[data-tab='${tabElement.dataset.tab}']`)
+    console.log(this.tabData);
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
-     
+    
     // Check to see if this.tabData is equal to 'all'
-    if(this.tabData === "all"){
+    if(this.tabData === 'all' ){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll(".card") ;
+      this.cards = document.querySelectorAll('.card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
-       
+      this.cards = document.querySelectorAll(`.card[data-tab='${tabElement.dataset.tab}']`);
     }
-     
+   
 
-     // Map over the newly converted NodeList we just created in our if statement above. 
-     //Convert each this.cards element into a new instance of the TabCard class. 
-     //Pass in a card object to the TabCard class. 
-     this.cards = Array.from(this.cards).map(x => new TabCard(x));
-     
+     // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
 
     // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
-    
   }
 
   selectTab(){
@@ -37,42 +33,37 @@ class TabLink {
     const tabs = document.querySelectorAll('.tab');
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    tabs.forEach(tab => tab.classList.remove('.active-tab'));
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
 
     // Select all of the elements with the .card class on them
-     const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card');
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    cards.forEach(card => {card.style.diplay = "none"});
+    cards.forEach(card => card.style.display = 'none');
     
     // Add a class of ".active-tab" to this.tabElement
-    this.tabElement.classList.add(".active-tab");  
+    this.tabElement.classList.add('active-tab');
+  
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-     this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-   this.cardElement = cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    this.cardElement.style.display = "flex";
+    this.cardElement.style.display = 'flex';
   }
 
 }
 
 /* START HERE: 
-
 - Select all classes named ".tab" and assign that value to the tabs variable
-
 - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
 - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
 */
-let tabs = document.querySelectorAll(".tab").forEach( newsTab =>{
-  new TabLink(newsTab)
-});
+let tabs = document.querySelectorAll('.tab').forEach( link => new TabLink(link));
